c788c2fae68ebb8e02b3472f759c6e4e3e312a2b
Zhentian Wan <answer881215@gmail.com>
2017-02-14 21:15:00 +0200

    connect in addtodo

---
+++ src/todos/containers/add-todo-context.container.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+let nextTodoId = 0;
+
+export let AddTodo = ({ dispatch }) => {
+    let input;
+
+    const onButtonClick = () => {
+        dispatch({
+                     type: 'ADD_TODO',
+                     payload: {
+                         id: nextTodoId++,
+                         name: input.value
+                     }
+                 });
+        input.value = "";
+    };
+
+    return (
+        <div>
+            <input type="text" ref={node => input = node}/>
+            <button
+                onClick={onButtonClick}
+            >Add todo
+            </button>
+        </div>
+    );
+};
+
+/*
+AddTodo = connect(
+    state => { return {}; },
+    dispatch => ({ dispatch})
+)(AddTodo);
+*/
+
+/*
+* Cause Context is not a stable API, we can avoid to using it.
+* SO here we also use connect from 'react-redux'.
+* We can reassign AddTodo = connect()(AddTodo);
+* It will pass the dispatch function to AddTodo's props.
+*
+* In AddTodo Component, we don't need state, just need dispatch function.
+* SO we can write:
+* AddTodo = connect(
+*   null,
+*   dispatch => ({dispatch})
+* )(AddTodo)
+*
+* And by default, connect function will link dispatch function to props,
+* so it means we even don't need to pass the dispatch function
+* SO we can write:
+* AddTodo = connect(null, null)(AddTodo);
+*
+* In short, we can write:
+* AddTodo = connect()(AddTodo);
+* */
+
+AddTodo = connect()(AddTodo); 
