c86473eb04f9f4090f5aadf9ef8faf9cc8917e2f
Zhentian Wan <answer881215@gmail.com>
2017-02-23 14:40:44 +0200

    fix add todo error and add animation

--- src/todos/components/todo-list.component.js
+++ src/todos/components/todo-list.component.js
@@ -4,11 +4,13 @@ import {Todo} from './todo.component';
 export const TodoList = ({todos, onTodoClick}) => {
     const list = todos.map(
         todo => (
+
             <Todo
                 {...todo}
                 key={todo.id}
                 onClick={() => onTodoClick(todo.id)}>
             </Todo>
+
         )
     );
     return (
